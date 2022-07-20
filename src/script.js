bd98ddfd52f0a4708d0fff302b4abe75a07de6d0
Daniel Matienzo <99377864+Daniel-Mat@users.noreply.github.com>
2022-07-20 02:14:13 -0700

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,25 @@
+window.addEventListener('DOMContentLoaded', () => {
+    VANTA.BIRDS({
+        el: "#vanta",
+        mouseControls: true,
+        touchControls: true,
+        gyroControls: true,
+        minHeight: 200.00,
+        minWidth: 200.00,
+        scale: 1.00,
+        scaleMobile: 1.00,
+        backgroundAlpha: 0.0,
+        color2: 0xff00f0,
+        wingSpan: 40.00,
+        separation: 100.00,
+        alignment: 77.00,
+        cohesion: 5.00,
+        quantity: 1.00
+    })
+
+    setTimeout(() => {
+        const main = document.querySelector('main')
+        main.style.opacity = 1
+        main.style.filter = 'blur(0px)'
+    }, 1000)
+})
